e200b1dbaa842b575ca1f600fcaa2b3b5190379f
Des Hartman <des@deshartman.com>
2022-05-27 10:54:35 +1000

    changed outbound URI to E164

--- functions/outboundHandler.protected.js
+++ functions/outboundHandler.protected.js
@@ -3,9 +3,13 @@
  */
 exports.handler = function (context, event, callback) {
 
+    // Convert SIP URI to +E.614
+    let to = event.To.match(/^sip:((\+)?[0-9]+)@(.*)/)[1];
+    let from = event.From.match(/^sip:((\+)?[0-9]+)@(.*)/)[1];
+
     const voiceResponse = new Twilio.twiml.VoiceResponse();
-    const dial = voiceResponse.dial({ callerId: event.From });
-    dial.number(event.To);
+    const dial = voiceResponse.dial({ callerId: from });
+    dial.number(to);
 
     return callback(null, voiceResponse);
 };
